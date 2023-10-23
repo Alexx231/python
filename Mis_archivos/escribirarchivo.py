b9113735b70b8ea079cffd67eb686ab6e7e3cf9e
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-23 07:31:52 +0000

    xd

---
+++ Mis_archivos/escribirarchivo.py
@@ -0,0 +1,12 @@
+#Script que abre y escribe en un archivo de texto
+archivo = open("mi_archivo.txt", "w")
+
+
+archivo.write("Hola Mundo")
+
+
+
+archivo.close
+
+
+#Crea un archivo de texto y escribir dentro una lista
