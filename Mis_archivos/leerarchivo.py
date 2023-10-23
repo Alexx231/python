91b4f06dbba871f901bd77ce46f421f073efee8c
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-23 09:36:07 +0000

    xxd

--- Mis_archivos/leerarchivo.py
+++ Mis_archivos/leerarchivo.py
@@ -11,10 +11,13 @@ archivo.close()
 #Leer listas desde un archivo de texto
 lista = []
 with open("frutas.txt","r") as archivo:
-    frutas = archivo.readline()
+    frutas = archivo.readlines()
+    
     
     #Utilizamos el metodo .strip para eliminar todos los espacios iniciales y finales, 
     #Asi como los tabuladores o saltos de linea del string
+   
+   
     for fruta in frutas:
         lista.append(fruta.strip())
     
---
+++ Mis_archivos/modificararchivo.py
@@ -0,0 +1,8 @@
+with open("mi_archivo.txt", "a") as archivo:
+    archivo.write("\nAdios mundo cruel")
+    
+archivo.close()
+
+
+#Modificar un archivo que contenga una lista
+
