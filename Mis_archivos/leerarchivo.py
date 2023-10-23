dfde2c8b5e9ac4d85f1ffb02d43bb2e5258cd2a3
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-23 08:05:14 +0000

    xd

--- Mis_archivos/leerarchivo.py
+++ Mis_archivos/leerarchivo.py
@@ -1,7 +1,25 @@
 #Script para leer archivos de texto
+lista = []
 
-with open("mi_archivo.txt", "r") as archivo:
+
+with open("mi_archivo.txt","r") as archivo:
     contenido =  archivo.read()
-    print (contenido)
+    print(contenido)
+      
+archivo.close()
+
+#Leer listas desde un archivo de texto
+
+with open("frutas.txt","r") as archivo:
+    frutas = archivo.readline()
     
-archivo.close
+    
+    for fruta in frutas:
+        lista.append(fruta)
+    
+    
+    print(frutas)
+    
+    
+    print("\nEsta es la lista llamada lista", lista)
+    
