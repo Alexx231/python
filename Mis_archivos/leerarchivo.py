250c7e20bb9ac0e77801bffaf29cbc807aa80008
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-23 08:12:01 +0000

    xd

--- Mis_archivos/leerarchivo.py
+++ Mis_archivos/leerarchivo.py
@@ -1,21 +1,22 @@
 #Script para leer archivos de texto
-lista = []
 
 
-with open("mi_archivo.txt","r") as archivo:
+
+with open('mi_archivo.txt','r') as archivo:
     contenido =  archivo.read()
     print(contenido)
       
 archivo.close()
 
 #Leer listas desde un archivo de texto
-
+lista = []
 with open("frutas.txt","r") as archivo:
     frutas = archivo.readline()
     
-    
+    #Utilizamos el metodo .strip para eliminar todos los espacios iniciales y finales, 
+    #Asi como los tabuladores o saltos de linea del string
     for fruta in frutas:
-        lista.append(fruta)
+        lista.append(fruta.strip())
     
     
     print(frutas)
