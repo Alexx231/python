c15a1a4008299a1da6ce3065bc689bb7ddae87b1
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-24 11:41:59 +0000

    xd

--- Mis_archivos/diario/diario.py
+++ Mis_archivos/diario/diario.py
@@ -1,1 +1,36 @@
-diario =
+diario = []
+
+
+def menu():
+    print(f"\n1.Añadir al diario")
+    print(f"\n2.Ver el diario")
+    print(f"\n3.Eliminar del diario")
+    print(f"\n4.Salir")
+    opcion = input(f"\nElige una opcion : ")
+    return opcion
+
+
+def anadir_diario():
+    
+
+
+def ver_diario():
+    
+
+def eliminar_diario():
+
+
+while True:
+    opcion = menu()
+    
+    if opcion == 1:
+        anadir_diario()
+    elif opcion == 2:
+        ver_diario()
+    elif opcion == 3:
+        eliminar_diario()
+    elif opcion == 4:
+        print(f"\nHASTA LA PROXIMA")
+        break
+    else:
+        print(f"\nIntentalo de nuevo")
