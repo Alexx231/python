cb1ddfe02ad5eba28ac5bb46069ef0e8f9dccba2
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-28 12:45:47 +0000

    xd

--- Mis_archivos/biblioteca_libros/biblioteca_libros.py
+++ Mis_archivos/biblioteca_libros/biblioteca_libros.py
@@ -1,6 +1,6 @@
 """
 
- Desarrollar un programa que permita gestionar los libros de una biblioteca.
+Desarrollar un programa que permita gestionar los libros de una biblioteca.
  
 Agregar un libro: Cada libro debe tener un título, autor, año de publicación y género.
 
@@ -17,4 +17,16 @@ Devolver un libro: Registrar cuándo un libro ha sido devuelto.
 Visualizar libros prestados: Mostrar una lista de todos los libros que están prestados y quién los tiene.
 
 """
-ifhsdifhsij
+
+def agregarlibro():
+    try:
+        Titulo = input(f"\nDime el titulo del libro : ")
+        Autor = input(f"\nDame el autor del libro : ")
+        Año_de_publicacion = input(f"\nEn que año se publico el libro? : ")
+        Genero = input(f"\nDime el genero del libro : ")
+        with open("biblioteca.txt","w") as archivo:
+            archivo.write(Titulo + ";" + Autor + ";" + Año_de_publicacion + ";" + Genero + "\n")
+            print(f"\nEl libro {Titulo} fue añadido correctamente")
+    except FileNotFoundError:
+        print(f"\nNo se pudo añadir el libro")
+    return
