b83191989ed1a6cd81998fafffeadf3dc90315d3
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-28 12:23:53 +0000

    xd

---
+++ Mis_archivos/biblioteca_libros/biblioteca_libros.py
@@ -0,0 +1,19 @@
+"""
+
+ Desarrollar un programa que permita gestionar los libros de una biblioteca.
+ 
+Agregar un libro: Cada libro debe tener un título, autor, año de publicación y género.
+
+Visualizar todos los libros: Mostrar una lista de todos los libros en la biblioteca.
+
+Buscar un libro por título o autor: Permitir al usuario buscar un libro específico.
+
+Eliminar un libro: Permitir al usuario eliminar un libro de la biblioteca.
+
+Prestar un libro: Registrar cuándo un libro ha sido prestado y a quién.
+
+Devolver un libro: Registrar cuándo un libro ha sido devuelto.
+
+Visualizar libros prestados: Mostrar una lista de todos los libros que están prestados y quién los tiene.
+
+"""
