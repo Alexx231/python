7c88ce1f0db33dd9f37dcbba4e8a84e784285d7c
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-28 13:03:27 +0000

    dd

--- Mis_archivos/biblioteca_libros/mod_biblioteca.py
+++ Mis_archivos/biblioteca_libros/mod_biblioteca.py
@@ -54,26 +54,3 @@ def prestarlibro():
 def devolverlibro():
     
 def visualizarlibrosprestados():
-
-
-while True:
-    opcion = menu()
-    
-    if opcion == 1:
-        Biblioteca = añadirlibro()
-    elif opcion == 2:
-        visualizarlibros()
-    elif opcion == 3:
-        buscarlibros()
-    elif opcion == 4:
-        Biblioteca = eliminarlibros()
-    elif opcion == 5:
-        Biblioteca = prestarlibro()
-    elif opcion == 6:
-        Biblioteca = devolverlibro()
-    elif opcion == 7:
-        visualizarlibrosprestados()
-        break
-    else:
-        print(f"\nLa opcion seleccionada no es valida ")
-    
