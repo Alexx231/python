c2548c5061b7419d6db045b8c1f10b97f647d36e
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-11-08 13:30:55 +0000

    xd

--- Mis_archivos/main_clientes.py
+++ Mis_archivos/main_clientes.py
@@ -7,7 +7,7 @@ crud_clientes.crear_clientes("Gonzalo, 36")
 crud_clientes.leer_clientes()
 
 
-crud_clientes.actualizar_clientes(36,"Gonzalo",)
+crud_clientes.actualizar_clientes(36,"Angel, 43")
 
 
-crud_clientes.borrar_clientes("La casa de Pepe, 209")
+crud_clientes.borrar_clientes("Gonzalo, 36")
