9ae01afd1b938b7f28eff68f1644bb9631e7c76e
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-11-14 13:21:06 +0000

    xd

--- hito_programacion/compras.py
+++ hito_programacion/compras.py
@@ -1,9 +1,13 @@
+from clientes import *
+from articulos import *
+
 compras = []
 
 def seguimiento_compra():
     print(f"\n¡Gracias por tu compra! Tu pedido ha sido procesado.")
     
 def realizar_compra():
+    ver_articulos()
     nombre_cliente = input(f"Dime el nombre del cliente que esta realizando la compra:")
     producto_a_comprar = input(f"Que producto desea comprar?:")
     unidades = input(f"Cuantas unidades desea comprar?:")
@@ -14,9 +18,9 @@ def realizar_compra():
                 producto = linea.strip().split(",")
                 if producto_a_comprar in producto[1]:
                     with open("compras.txt","a") as archivo:
-                         archivo.write(f"{nombre_cliente} - {unidades} - {producto[1]}\n")
-                         print(f"\nEl cliente{nombre_cliente} ha comprado {unidades} unidades de{producto[1]}\n")
-                         seguimiento_compra()
+                        archivo.write(f"{nombre_cliente} - {unidades} - {producto[1]}\n")
+                        print(f"\nEl cliente{nombre_cliente} ha comprado {unidades} unidades de{producto[1]}\n")
+                    seguimiento_compra()
     except FileNotFoundError:
         print(f"No se pudo realizar la compra")
     return
