a65ee64a66a6d4b47cfc46a2d1ad1b6fc69dd96c
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-11-21 13:38:15 +0000

    xd

--- ejercicio_crud/bbd_empresa.py
+++ ejercicio_crud/bbd_empresa.py
@@ -1,1 +1,14 @@
 import mysql.connector
+
+def conexion_bdd():
+    try:
+        return mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="curso",
+            database=""
+            )
+    except mysql.connector.Error as e:
+        print(f"\nLa conexion no pudo ser realizada",e) 
+
+conexion_bdd()
