cafd71bc1e532003fc9438e3292ea0a27f1dfe64
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2024-01-09 11:20:51 +0100

    Add files via upload

---
+++ dibujo.py
@@ -0,0 +1,11 @@
+"""
+This module generates ASCII art using the 'art' library.
+"""
+
+import art
+
+# Generate ASCII art of Bart Simpson
+bart_ascii = art.text2art("si")
+# Print the ASCII art
+print(bart_ascii)
+
